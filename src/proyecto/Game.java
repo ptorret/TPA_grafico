55927277a8ca45ab43d6f9ad6352afa64e87ae77
Pierott1 <126704798+Pierott1@users.noreply.github.com>
2023-10-17 18:09:50 +0200

    creation

---
+++ src/proyecto/Game.java
@@ -0,0 +1,23 @@
+package proyecto;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel {
+    int x = 0;
+    int y = 0;
+    public void moveBall(){
+        x = x + 1;
+        y = y + 1;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        g2D.fillOval(x,y,30,30);
+    }
+
+
+}
