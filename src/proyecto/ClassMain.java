674cb12881f3eea9a5a98a11e352f01a4aa86bd1
Pierott1 <126704798+Pierott1@users.noreply.github.com>
2023-10-17 17:44:55 +0200

    creation

---
+++ src/proyecto/ClassMain.java
@@ -0,0 +1,21 @@
+package proyecto;
+
+import javax.swing.*;
+
+public class ClassMain{
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Mini");
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300,400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while(true){
+            game.moveBall();
+            game.repaint();
+            Thread.sleep(10);
+        }
+    }
+}
